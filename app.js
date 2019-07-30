c9da0c4cb04f760a128ea6256afd17ad9b0acce4
bob <bob@orbilabs.com>
2019-07-30 12:27:46 -0400

    first commit sans history

---
+++ app.js
@@ -0,0 +1,71 @@
+'use strict'
+const path = require('path')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const compression = require('compression')
+const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
+const app = express()
+const router = express.Router()
+
+const createError = require('http-errors');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+var mapRouter = require('./routes/map');
+var convertRouter = require('./routes/convertdata');
+
+// view engine setup
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'));
+
+if (process.env.NODE_ENV === 'test') {
+  // NOTE: aws-serverless-express uses this app for its integration tests
+  // and only applies compression to the /sam endpoint during testing.
+  router.use('/sam', compression())
+} else {
+  router.use(compression())
+}
+
+router.use(cors())
+router.use(bodyParser.json())
+router.use(bodyParser.urlencoded({ extended: true }))
+router.use(awsServerlessExpressMiddleware.eventContext())
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', mapRouter);
+app.use('/convert', convertRouter);
+
+// error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+//
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+
+router.get('/', (req, res) => {
+  res.render('index', {
+    apiUrl: req.apiGateway ? `https://${req.apiGateway.event.headers.Host}/${req.apiGateway.event.requestContext.stage}` : 'http://localhost:3000'
+  })
+})
+
+router.get('/sam', (req, res) => {
+  res.sendFile(`${__dirname}/sam-logo.png`)
+})
+
+// The aws-serverless-express library creates a server and listens on a Unix
+// Domain Socket for you, so you can remove the usual call to app.listen.
+// app.listen(3000)
+//app.use('/', router)
+
+// Export your express server so you can import it in the lambda function.
+module.exports = app
---
+++ routes/map.js
@@ -0,0 +1,173 @@
+var express = require('express');
+var router = express.Router();
+var query_overpass = require('query-overpass');
+var request = require("request");
+
+console.log("Launching Thirsty Ottawa...");
+//console.log("Args: " + process.argv);
+
+var queryOpendataDrinkingWater=false;
+var queryOpenStreetmapToilets=true;
+var queryOpenStreetmapDrinkingWater=true;
+
+var odoData = [];
+var osmData = [];
+
+var googleapikey = "AIzaSyAzf46rIYu-VSBJFixUYZwz__jpAN1Qqjw";
+
+// parse variables
+const yargs = require('yargs');
+const argv = yargs
+    .option('googleapikey', {
+        alias: 'g',
+        description: 'Override built-in google api key.',
+        type: 'String',
+    })
+    .argv;
+
+if(argv.googleapikey) {
+  googleapikey = argv.googleapikey;
+  console.log("Overriding google api key with: " + googleapikey);
+}
+
+
+
+// this will get data from open data ottawa
+// this is disabled by default as all the data is in open street maps (as of Jul 30, 2019)
+// can load via:
+// http://localhost:3000/thirsty?fetchodo=1
+// note that this data should not be cached.
+function fetchFromOpendataOttawa(res, onCompletion) {
+  var ottawaDataUrl = "http://data.ottawa.ca/api/action/datastore_search?resource_id=1832175d-54e9-422d-8e63-705f157b7352"; //&limit=5"
+
+  request(ottawaDataUrl, function (error, response, body) {
+    console.error('error:', error);
+    console.log('statusCode:', response && response.statusCode);
+
+    var odoResult = JSON.parse(body);
+    var fetchedOdoData = odoResult['result']['records'];
+    if(fetchedOdoData) {
+      odoData = fetchedOdoData;
+    }
+    //console.log('odo data: ', JSON.stringify(odoData));
+    onCompletion(res);
+  });
+}
+
+// render the map using pug - and yes, this is MY api key, don't
+// bother trying to steal and spam with it, it's use is geofenced
+function renderMap(res) {
+  res.render('map', {
+    title: 'Drinking Fountains of Ottawa',
+    OSMDATA: JSON.stringify(osmData['features']),
+    OPODATA: JSON.stringify(odoData),
+    GOOGLEAPIKEY: googleapikey});
+}
+
+// cache the odo data for a period of time
+var osmDataCache = null;
+var osmDataCacheDate = null;
+var osmDataTTL = 60 * 1000;
+
+// pull the open street map data from the overpass api
+function fetchOverpassData(req, res, onCompletion) {
+  console.log("Request received, about to query overpass...");
+
+  // a decent sized square around ottawa area,
+  // query for the drinking water OR toilet amenities
+  var ql = 'node(45.1,-75.8,45.5,-75.5)[amenity~"drinking_water|toilets"];out meta;';
+
+  // minor hack to load toronto
+  if(req.query.toronto) {
+    var toronto = [43.661768, -79.425454];
+    ql = 'node(' + (toronto[0] - 0.3) +
+      ',' + (toronto[1] - 0.2) +
+      ',' + (toronto[0] + 0.3) +
+      ',' + (toronto[1] + 0.2) +
+      ')[amenity~"drinking_water|toilets"];out meta;';
+  }
+
+  query_overpass(ql, function(error, fetchedOsmData) {
+    console.log("overpass query done, error: " + error); // + ", keylen: " + Object.keys(fetchedOsmData).length);
+    osmData = fetchedOsmData;
+    if(req.query.dumptoconsole) {
+      console.log("osmdata w meta: " + JSON.stringify(osmData));
+    }
+
+    if(error) {
+      console.log("Error from overpass, not updating cache.");
+    } else if(allowCache(req)) {
+      // update the cache
+      osmDataCache = osmData;
+      osmDataCacheDate = Date.now();
+      console.log("Cache updated as of now: " + osmDataCacheDate);
+    }
+
+    onCompletion(req, res);
+  });
+}
+
+// after we have (either via cache or by pulling it) the open street map data
+// from overpass, either carry on with render, or get open street map data
+function afterOSM(req, res) {
+  console.log("request query.fetchodo: " + req.query.fetchodo);
+  if(queryOpendataDrinkingWater || (req.query.fetchodo && req.query.fetchodo == true)) {
+    console.log("Asked to fetch open data ottawa data.");
+    fetchFromOpendataOttawa(res, renderMap);
+  } else {
+    console.log("Not getting open data ottawa data.");
+    renderMap(res);
+  }
+}
+
+function allowCache(req) {
+  // only allow cache if other funky options not chosen
+  return !req.query.toronto;
+}
+
+
+//
+// main GET method, entry point for the whole shabang
+//
+
+var cacheRequestInProgress = false;
+router.get('/', function(req, res, next) {
+
+  // if we have any data cached at all, we use it, then spawn to update.
+  // this way only the first request ever is slow.
+  if(allowCache(req) && osmDataCache) {
+
+    var ttl = Date.now() - osmDataTTL;
+    if(!osmDataCacheDate || (osmDataCacheDate < ttl)) {
+      console.log("cache is expired at " + ttl + ", may query OSM synchronously, in progress: " + cacheRequestInProgress);
+      if(!cacheRequestInProgress) {
+        console.log("Making cache request.");
+        cacheRequestInProgress = true;
+        fetchOverpassData(req, res, () => {
+          // do nothing!
+          console.log("Done filling the cache.");
+          cacheRequestInProgress = false;
+        });
+      } else {
+        console.log("Not making cache request, one in progress.");
+      }
+    } else {
+      console.log("cache is still good til " + ttl + ", not querying OSM asynchronously");
+    }
+
+    // load from the cache
+    osmData = osmDataCache;
+
+    // and proceed
+    afterOSM(req, res);
+
+  } else {
+
+    // call as per normal, there is no cache we can use
+    console.log("Cache isn't active, going sync, last load date: " + osmDataCacheDate);
+    fetchOverpassData(req, res, afterOSM);
+  }
+
+});
+
+module.exports = router;
